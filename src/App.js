7855a48e857ff4fe20241ebd62d5990e6bc5d2c7
[Sahil Thapa] <s2300123@BC-C02GQ5FAQ6LR-MacBook-Air.local>
2023-03-23 12:57:38 +0200

    read me updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am <code>src/App.js</code> taking over from here.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        See you
         </a>
       </header>
     </div>
